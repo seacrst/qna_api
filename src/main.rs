9f0ee58e3c004cacb553b58ec0ff7d3170fa36e0
Seacrest <paganseacrest@gmail.com>
2024-04-21 00:52:56 +0300

    api app

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,65 @@
+#[macro_use]
+extern crate log;
+
+extern crate pretty_env_logger;
+
+use std::sync::Arc;
+
 use axum::{
-    routing::{get, post, delete},
-    Router
+    routing::{delete, get, post},
+    Router,
 };
-use dotenv;
+use dotenvy::dotenv;
+
+use persistance::{
+    answers_dao::{AnswersDao, AnswersDaoImpl},
+    questions_dao::{QuestionsDao, QuestionsDaoImpl},
+};
 use sqlx::postgres::PgPoolOptions;
 
-use crate::handlers::*;
-
 mod handlers;
+mod models;
 mod persistance;
-mod dto;
 
-const PORT:u16 = 7878;
+use handlers::*;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub questions_dao: Arc<dyn QuestionsDao + Send + Sync>,
+    pub answers_dao: Arc<dyn AnswersDao + Send + Sync>,
+}
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    dotenv::dotenv().ok();
-    
+    dotenv().ok();
+
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set")).await
-        .expect("Failed to create postgres connection pool");
-    
-    let question = Question::config();
-    let answer = Answer::config();
+        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set."))
+        .await
+        .expect("Failed to create Postgres connection pool!");
 
-    let routes = Router::new()
-        .route(question.questions_path, get(Question::read))
-        .route(question.question_path, post(Question::create))
-        .route(question.question_path, delete(Question::delete))
-        .route(answer.answers_path, get(Answer::read))
-        .route(answer.answer_path, post(Answer::create))
-        .route(answer.answer_path, delete(Answer::delete));
+    let questions_dao = Arc::new(QuestionsDaoImpl::new(pool.clone()));
+    let answers_dao = Arc::new(AnswersDaoImpl::new(pool));
 
+    let app_state = AppState {
+        questions_dao,
+        answers_dao,
+    };
 
-    let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{PORT}"))
+    let app = Router::new()
+        .route("/question", post(create_question))
+        .route("/questions", get(read_questions))
+        .route("/question", delete(delete_question))
+        .route("/answer", post(create_answer))
+        .route("/answers", get(read_answers))
+        .route("/answer", delete(delete_answer))
+        .with_state(app_state);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:7878")
         .await
         .unwrap();
 
-    axum::serve(listener, routes).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
